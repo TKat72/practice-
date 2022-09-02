cc8122dfdeabc4423e7287bc4b242064906ff97d
Kateryna <tsymbal.katena@mail.ru>
2022-09-01 23:44:14 -0500

    optimezed solution

--- nums.js
+++ nums.js
@@ -1,22 +1,22 @@
 var twoSum = function (nums, target) {
     let temp;
     let result = [];
-    for (let i = 0; i < nums.length; i++) {
-        console.log("im here ", nums[i])
-        temp = nums[i];
-        for (let j = 1; j < nums.length; j++) {
-            console.log("im in for 2 ", nums[j])
-            if (temp + nums[j] === target) {
-                result.push(i)
-                result.push(j)
-                console.log(result)
-                return temp;
-            }
+    for (let i = 0; i < nums.length - 1; i++) {
 
-        }
+
 
     }
+    // for (let j = 1; j < nums.length; j++) {
+    //     console.log("im in for 2 ", nums[j])
+    //     if (temp + nums[j] === target) {
+    //         result.push(i)
+    //         result.push(j)
+    //         console.log("Im here 3", result)
+    //         return temp;
+    //     }
+
+    // }
 
 };
 
-twoSum([2, 7, 11, 15], 9)
+twoSum([2, 5, 5, 11], 10)
