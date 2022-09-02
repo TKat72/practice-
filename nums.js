4f7e204d92b7424e740fec5fcd1e00ad44278188
Kateryna <tsymbal.katena@mail.ru>
2022-09-01 23:49:08 -0500

    onather way

--- nums.js
+++ nums.js
@@ -3,7 +3,13 @@ var twoSum = function (nums, target) {
     let result = [];
     for (let i = 0; i < nums.length - 1; i++) {
 
-
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                result.push(i)
+                result.push(j)
+                console.log(result)
+            }
+        }
 
     }
     // for (let j = 1; j < nums.length; j++) {
