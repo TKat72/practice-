3d1dbed06c74021e00c1a96700e965164a62c262
Kateryna <tsymbal.katena@mail.ru>
2022-09-03 17:18:58 -0500

    two nums

--- nums.js
+++ nums.js
@@ -8,6 +8,7 @@ var twoSum = function (nums, target) {
                 result.push(i)
                 result.push(j)
                 console.log(result)
+                return result;
             }
         }
 
@@ -26,3 +27,33 @@ var twoSum = function (nums, target) {
 };
 
 twoSum([2, 5, 5, 11], 10)
+
+
+function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+
+  /**
+   * @param {ListNode} l1
+   * @param {ListNode} l2
+   * @return {ListNode}
+   */
+  var addTwoNumbers = function (l1, l2) {
+    let carry = 0;
+    let additionNode = new ListNode(0);
+    let ansNode = additionNode;
+
+    while (l1 || l2 || carry) {
+      let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+      carry = Math.floor(sum / 10);
+      sum %= 10;
+      ansNode.next = new ListNode(sum);
+
+      l1 = l1 ? l1.next : null;
+      l2 = l2 ? l2.next : null;
+      ansNode = ansNode.next;
+    }
+
+    return additionNode.next;
+  };
