db85834aa26fe7432abf45bfe470794e6f173ee3
Kateryna <tsymbal.katena@mail.ru>
2022-09-29 01:56:48 -0500

    counting big o) git push

--- nemo.js
+++ nemo.js
@@ -6,14 +6,39 @@ function findNemo(array) {
     let count = 0
     for (let i = 0; i < array.length; i++) {
         if (array[i] === "nemo") {
-            console.log("find Nemo")
+
             return " hello"
 
         }
-        console.log(count++)
+
     }
     let t1 = performance.now()
     console.log("call ti find nemo took ", t1 - t0, "mill")
 }
 
 findNemo(large);
+
+function logFirstTwo(arr) {
+    console.log(arr[0] + "1")
+    console.log(arr[1] + "2")
+}
+logFirstTwo(large)
+
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function anotherFunChallenge(input) {
+    let a = 5;//o(1)
+    let b = 10;//o(1)
+    let c = 50;//o(1)
+    for (let i = 0; i < input; i++) {
+        let x = i + 1;//O(n)
+        let y = i + 2;//O(N)
+        let z = i + 3;// o(n)
+
+    }
+    for (let j = 0; j < input; j++) {
+        let p = j * 2;// o(n)
+        let q = j * 2;// o(n)
+    }
+    let whoAmI = "I don't know";// o(1)
+    // ( 1 + 1 +1 + n+n+n +n +n + 1) = o(n)
+}
