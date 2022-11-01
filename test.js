4036563e95d3cdc6f9f8035a0d2c7d956d90bbf0
Kateryna <tsymbal.katena@mail.ru>
2022-11-01 09:42:04 -0500

    merge two serted lists

--- test.js
+++ test.js
@@ -532,3 +532,16 @@ function threeSum(nums) {
 
     return results
 };
+
+// merge two sereted lists into onevar
+ mergeTwoLists = function (l1, l2) {
+    if (!l1) return l2;
+    else if (!l2) return l1;
+    else if (l1.val <= l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2
+    }
+};
